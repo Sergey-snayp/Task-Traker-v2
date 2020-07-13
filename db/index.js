c4f90d0ed44c83ca0e20fca57d6a7eeb36d862cb
Pronyuk Sergey <sergeypronyuk@gmail.com>
2020-07-13 19:03:50 +0300

    edit config settings edit connect to database

--- db/index.js
+++ db/index.js
@@ -2,13 +2,7 @@ const { Pool } = require('pg');
 const config = require('config');
 
 const pool = new Pool(
-  {
-    user: config.DB_USER,
-    host: config.DB_HOST,
-    database: config.DB_DATABASE,
-    password: config.DB_PASSWORD,
-    port: config.DB_PORT,
-  },
+  { ...config.postgres },
 );
 module.exports = {
   query: (text, params) => pool.query(text, params),
