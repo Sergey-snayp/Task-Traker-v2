abc637aaa69d833e8a0609b7105750ab42bc1977
Pronyuk Sergey <sergeypronyuk@gmail.com>
2020-07-13 17:05:27 +0300

    Database operation through the config

--- db/index.js
+++ db/index.js
@@ -1,12 +1,13 @@
 const { Pool } = require('pg');
+const config = require('config');
 
 const pool = new Pool(
   {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'testdb',
-    password: '1111',
-    port: 5432,
+    user: config.DB_USER,
+    host: config.DB_HOST,
+    database: config.DB_DATABASE,
+    password: config.DB_PASSWORD,
+    port: config.DB_PORT,
   },
 );
 module.exports = {
